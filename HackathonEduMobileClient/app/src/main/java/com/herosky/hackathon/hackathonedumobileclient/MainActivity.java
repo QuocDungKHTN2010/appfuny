fe670ebd3051c8c5b7593fb96af30547de776cad
kduy <kduyit92@gmail.com>
2015-08-01 15:25:40 +0700

    Android source

---
+++ HackathonEduMobileClient/app/src/main/java/com/herosky/hackathon/hackathonedumobileclient/MainActivity.java
@@ -0,0 +1,16 @@
+package com.herosky.hackathon.hackathonedumobileclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DELL on 8/1/2015.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.mainlayout);
+        super.onCreate(savedInstanceState);
+    }
+}
